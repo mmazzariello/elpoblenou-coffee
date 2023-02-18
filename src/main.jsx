f1471ecc271cd5f13f685b56771588e1c8315fb9
Mariana Mazza <mariana.mazzariello@bolttech.io>
2023-02-18 17:45:15 +0100

    INSTALL and INITIALIZE firebase in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import * as ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcMLZgQQ1syCtN8n9Vh7eR4JjwaxmI7E8",
+  authDomain: "speciality-coffee-91a8e.firebaseapp.com",
+  projectId: "speciality-coffee-91a8e",
+  storageBucket: "speciality-coffee-91a8e.appspot.com",
+  messagingSenderId: "167864496076",
+  appId: "1:167864496076:web:28f37072ec93de8c7ab769",
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
